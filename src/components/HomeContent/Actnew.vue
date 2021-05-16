b8ff52cf0888e08d6f2a9c11deeaa38aa7d4f000
Your Name <you@example.com>
2021-05-16 14:13:28 +0800

    form ok

--- src/components/HomeContent/Actnew.vue
+++ src/components/HomeContent/Actnew.vue
@@ -12,10 +12,13 @@
                 placeholder="淨灘活動"
                 />
             </div>
-            <div class="col-md-12">
-                <UploadImages   @change="handleImages" uploadMsg="請上傳活動圖片" :max="1"/>
+            <div class="form-group" style="margin-top:2rem;">
+                <label for="exampleInputEmail1">上傳圖片</label>
+                <div class="col-md-12">
+                    <UploadImages   @change="handleImages" uploadMsg="請上傳活動圖片" :max="1"/>
+                </div>
             </div>
-            <!-- <p>{{uploadImg}}</p> -->
+            
             <div class="form-group" style="margin-top:1rem;">
                 <label for="exampleInputEmail1">活動類別</label>
                 <div class="form-check form-check-inline">
@@ -50,154 +53,157 @@
                 >We'll never share your email with anyone else.</small
                 >
             </div>
-            <div class="form-group" style="margin-top:1rem;">
+            <div class="form-group" style="margin-top:1rem; ">
                 <label for="exampleInputEmail1">活動型態</label>
                 <div class="form-check form-check-inline">
-                <input
-                    class="form-check-input"
-                    type="radio"
-                    name="inlineRadioOptions"
-                    id="inlineRadio1"
-                    value="once"
-                    v-model="picked"
-                />
-                <label class="form-check-label" for="inlineRadio1">單次活動</label>
+                    <input
+                        class="form-check-input"
+                        type="radio"
+                        name="inlineRadioOptions"
+                        id="inlineRadio1"
+                        value="once"
+                        v-model="picked"
+                    />
+                    <label class="form-check-label" for="inlineRadio1">單次活動</label>
                 </div>
                 <div class="form-check form-check-inline">
-                <input
-                    class="form-check-input"
-                    type="radio"
-                    name="inlineRadioOptions"
-                    id="inlineRadio2"
-                    value="period"
-                    v-model="picked"
-                />
-                <label class="form-check-label" for="inlineRadio2">週期活動</label>
+                    <input
+                        class="form-check-input"
+                        type="radio"
+                        name="inlineRadioOptions"
+                        id="inlineRadio2"
+                        value="period"
+                        v-model="picked"
+                    />
+                    <label class="form-check-label" for="inlineRadio2">週期活動</label>
                 </div>
-                <span>Picked: {{ picked }}</span>
+                <!-- <span>Picked: {{ picked }}</span> -->
+                <div v-if="picked=='once'"  >
+                    <div class="form-group ">
+                        <label for="exampleInputEmail1">日期</label>
+                        <input
+                        type="email"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="2021/12/21"
+                        />
+                    </div>
+                
+                    <div class="form-inline ">
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" >開始時間</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" class="ml-xl-2" >結束時間</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group ">
+                            <label for="exampleInputEmail1">時數</label>
+                            <input
+                            type="email"
+                            class="form-control "
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="2021/12/21"
+                            />
+                    </div>
+                </div>
+                <div v-if="picked=='period'"  >
+                    <div class="form-inline ">
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" >開始日期</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" class="ml-xl-2" >結束日期</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group ">
+                        <label for="exampleInputEmail1">星期</label>
+                        <select class="custom-select">
+                            <option selected value="mon">星期一</option>
+                            <option value="tue">星期二</option>
+                            <option value="wed">星期三</option>
+                            <option value="thu">星期四</option>
+                            <option value="fri">星期五</option>
+                            <option value="sat">星期六</option>
+                            <option value="sun">星期日</option>
+                        </select>
+                    </div>
+                
+                    <div class="form-inline ">
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" >開始時間</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="exampleInputEmail1" class="ml-xl-2" >結束時間</label>
+                            <input 
+                                type="email"
+                                class="form-control ml-xl-2"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="2021/12/21"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group ">
+                            <label for="exampleInputEmail1">時數</label>
+                            <input
+                            type="email"
+                            class="form-control "
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="2021/12/21"
+                            />
+                    </div>
+                    <div class="form-group ">
+                            <label for="exampleInputEmail1">隔多少週</label>
+                            <input
+                            type="email"
+                            class="form-control "
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="2021/12/21"
+                            />
+                    </div>
+                </div>
             </div>
-            <div v-if="picked=='once'">
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">日期</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">開始時間</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                    <label for="exampleInputEmail1">結束時間</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">時數</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-            </div>
-            <div v-if="picked=='period'">
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">開始日期</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                    <label for="exampleInputEmail1">結束日期</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">星期</label>
-                    <select class="custom-select">
-                        <option selected value="mon">星期一</option>
-                        <option value="tue">星期二</option>
-                        <option value="wed">星期三</option>
-                        <option value="thu">星期四</option>
-                        <option value="fri">星期五</option>
-                        <option value="sat">星期六</option>
-                        <option value="sun">星期日</option>
-                    </select>
-                    <label for="exampleInputEmail1">開始時間</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                    <label for="exampleInputEmail1">結束時間</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">時數</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-                <div class="form-inline row">
-                    <label for="exampleInputEmail1">格多少週</label>
-                    <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="2021/12/21"
-                    />
-                </div>
-            </div>
-
-            <div class="form-group" style="margin-top:1rem;">
-                <label for="exampleInputEmail1">活動時間</label>
-                <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="2021/12/21"
-                />
-                <small id="emailHelp" class="form-text text-muted"
-                >We'll never share your email with anyone else.</small
-                >
-            </div>
+    
             <div class="form-group" style="margin-top:1rem;">
                 <label for="exampleInputEmail1">承辦人員</label>
                 <input
