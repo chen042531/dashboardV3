b04491bf9d89b1e53f686bbd03c602ae3c2c6ff4
ycchen <chen042531.cs03@nctu.edu.tw>
2021-05-23 18:48:30 +0800

    vue-chartjs rerender ok

--- src/components/HomeContent/chart/GenderRate.vue
+++ src/components/HomeContent/chart/GenderRate.vue
@@ -7,6 +7,35 @@ import { Bar, Line, Pie } from 'vue-chartjs'
 export default {
   extends: Pie,
   props:['gender_r'],
+  watch:{
+    gender_r:{
+        handler: function (newVal, oldVal) {
+           console.log('gender Prop changed: ', newVal, ' | was: ', oldVal);
+          this.renderChart({
+            labels: ["男", "女", "其他"],
+            datasets: [{
+                    data:  this.gender_r,
+                    backgroundColor: [
+                      'rgba(54, 162, 235)',
+                      'rgba(255, 99, 132)',
+                      'rgba(255, 206, 86)',
+                    ],
+                    borderWidth: 1
+                  }]
+            
+          })
+        },
+        deep: true
+
+    }
+  },
+  data () {
+    return {
+      options: {
+         /* my specific data */
+      }
+    }
+  },
   mounted () {
     // Overwriting base render method with actual data.
     console.log("ddddddddddddddddddddddddddddd",this.gender_r);
